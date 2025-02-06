051c6de2de3ff3c89c8cc1a614a8756b945eee4e
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-02-06 20:57:31 +0700

    add: form input

--- lib/widget/groceries_list.dart
+++ lib/widget/groceries_list.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/widget/new_item.dart';
 
-class GroceriesList extends StatelessWidget {
+class GroceriesList extends StatefulWidget {
   const GroceriesList({super.key});
+
+  @override
+  State<GroceriesList> createState() => _GroceriesListState();
+}
+
+class _GroceriesListState extends State<GroceriesList> {
+  void _addItem() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Your Groceries'),
+          actions: [
+            IconButton(onPressed: _addItem, icon: Icon(Icons.plus_one))
+          ],
         ),
         body: ListView.builder(
           itemCount: groceryItems.length,
-          itemBuilder: (context, index) => 
-          ListTile(
+          itemBuilder: (context, index) => ListTile(
             title: Text(groceryItems[index].name),
-            leading: Container(width: 24, height: 24, color: groceryItems[index].category.color),
+            leading: Container(
+                width: 24,
+                height: 24,
+                color: groceryItems[index].category.color),
             trailing: Text(groceryItems[index].quantity.toString()),
-            ),
+          ),
         ));
   }
 }
---
+++ lib/widget/new_item.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/categories.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add New Item'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: InputDecoration(label: Text('Name')),
+                validator: (value) => 'test',
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(label: Text('Quantity')),
+                      initialValue: '1',
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                SizedBox(width: 8),
+                                Text(category.value.title)
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: Text('Reset')),
+                  ElevatedButton(onPressed: () {}, child: Text('Submit'))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
