dd058572452a30e08467646b228f1cd67b29d24f
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-02-11 00:25:53 +0700

    add: example post new-item with http

--- lib/widget/groceries_list.dart
+++ lib/widget/groceries_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_app/data/dummy_items.dart';
 import 'package:shopping_list_app/models/grocery_item.dart';
 import 'package:shopping_list_app/widget/new_item.dart';
 
--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -1,9 +1,9 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
-import 'package:shopping_list_app/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,13 +21,19 @@ class _NewItemState extends State<NewItem> {
   var _selectedCategory = categories[Categories.vegetables]!;
 
   void _saveItem() {
-    _keyForm.currentState!.validate();
-    _keyForm.currentState!.save();
-    Navigator.of(context).pop<GroceryItem>(GroceryItem(
-        id: DateTime.now().toString(),
-        name: _enteredName,
-        quantity: _enterdQuantity,
-        category: _selectedCategory));
+    if (_keyForm.currentState!.validate()) {
+      final url = Uri.https(
+          'learning-28fcc-default-rtdb.firebaseio.com', 'shooping_list.json');
+      _keyForm.currentState!.save();
+      http.post(url,
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enterdQuantity,
+            'category': _selectedCategory.title
+          }));
+    }
+    // Navigator.of(context).pop<GroceryItem>();
   }
 
   @override
